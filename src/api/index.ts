ddf2e93e3d9dd0d330f98968fd643b4458846e4a
Serjge <serjge@yandex.ru>
2022-04-09 13:34:11 +0300

    refactoring

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,7 +4,7 @@ const server = process.env.APP_URL_EMAIL_SERVER as string
 export const API = {
   sendEmail(name: FormDataEntryValue | null, email: FormDataEntryValue | null, message: FormDataEntryValue | null)  {
     console.log(process.env.APP_URL_EMAIL_SERVER)
-    return axios.post( server , {
+    return axios.post( 'https://gmail-portfolio.herokuapp.com' as string , {
       name: name,
       email: email,
       message: message
